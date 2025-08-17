5e62fb994aa01d9afb08b7e218add1d81b018135
Bruno Eduardo <bruno.139737@gmail.com>
2025-08-17 00:31:55 +0000

    Adiciona o ClassSerializerInterceptor ao bootstrap do aplicativo e refatora o controlador de categorias para utilizar DTOs e serialização de saída. Implementa validação de UUIDs nos parâmetros de atualização e exclusão, além de ajustar os métodos de criação e atualização para retornar a saída serializada.

--- src/nest-modules/categories-module/categories.controller.ts
+++ src/nest-modules/categories-module/categories.controller.ts
@@ -1,11 +1,23 @@
-import { CreateCategoryInput } from '@core/category/application/use-cases/create-category/create-category.input';
+import { CategoryOutput } from '@core/category/application/use-cases/common/category-output';
 import { CreateCategoryUseCase } from '@core/category/application/use-cases/create-category/create-category.use-case';
 import { DeleteCategoryUseCase } from '@core/category/application/use-cases/delete-category/delete-category.use-case';
 import { GetCategoryUseCase } from '@core/category/application/use-cases/get-category/get-category.use-case';
 import { ListCategoriesUseCase } from '@core/category/application/use-cases/list-categories/list-categories.use-case';
-import { UpdateCategoryInput } from '@core/category/application/use-cases/update-category/update-category.input';
 import { UpdateCategoryUseCase } from '@core/category/application/use-cases/update-category/update-category.use-case';
-import { Body, Controller, Inject, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  HttpCode,
+  Inject,
+  Param,
+  ParseUUIDPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CategoryPresenter } from 'src/nest-modules/categories-module/categories.presenter';
+import { CreateCategoryDto } from 'src/nest-modules/categories-module/dto/create-category.dto';
+import { UpdateCategoryDto } from 'src/nest-modules/categories-module/dto/update-category.dto';
 
 @Controller('categories')
 export class CategoriesController {
@@ -25,18 +37,33 @@ export class CategoriesController {
   private listUseCase: ListCategoriesUseCase;
 
   @Post()
-  create(@Body() createCategoryDto: CreateCategoryInput) {
-    return this.createUseCase.execute(createCategoryDto);
+  async create(@Body() createCategoryDto: CreateCategoryDto) {
+    const output = await this.createUseCase.execute(createCategoryDto);
+    return CategoriesController.serialize(output);
   }
 
   @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateCategoryDto: UpdateCategoryInput,
+  async update(
+    @Param('id', new ParseUUIDPipe({ errorHttpStatusCode: 422 })) id: string,
+    @Body() updateCategoryDto: UpdateCategoryDto,
   ) {
-    return this.updateUseCase.execute({
+    const output = await this.updateUseCase.execute({
       ...updateCategoryDto,
       id,
     });
+
+    return CategoriesController.serialize(output);
+  }
+
+  @HttpCode(204)
+  @Delete(':id')
+  async delete(
+    @Param('id', new ParseUUIDPipe({ errorHttpStatusCode: 422 })) id: string,
+  ) {
+    return this.deleteUseCase.execute({ id });
+  }
+
+  static serialize(output: CategoryOutput) {
+    return new CategoryPresenter(output);
   }
 }
