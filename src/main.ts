baf5ed3fe872939747778361cf0ec250d3ec4908
Bruno Eduardo <bruno.139737@gmail.com>
2025-08-24 01:40:26 +0000

    Atualiza a configuração do Jest para testes E2E, substituindo o arquivo de configuração JSON por um arquivo TypeScript. Adiciona filtros globais de tratamento de erros no bootstrap do aplicativo, melhorando a gestão de erros. Ajusta o arquivo tsconfig.build.tsbuildinfo para refletir as mudanças nas dependências.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { NestFactory, Reflector } from '@nestjs/core';
 import { WrapperDataInterceptor } from 'src/nest-modules/shared-module/interceptors/wrapper-data/wrapper-data.interceptor';
+import { EntityValidationErrorFilter } from 'src/nest-modules/shared-module/filters/entity-validation-error.filter';
+import { NotFoundErrorFilter } from 'src/nest-modules/shared-module/filters/not-found-error.filter';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -14,6 +16,10 @@ async function bootstrap() {
 
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.useGlobalInterceptors(new WrapperDataInterceptor());
+  app.useGlobalFilters(
+    new NotFoundErrorFilter(),
+    new EntityValidationErrorFilter(),
+  );
 
   await app.listen(process.env.PORT ?? 3000);
 }
