4771e28fd7026256fa49bbed324c0b9fb58521ef
Bruno Eduardo <bruno.139737@gmail.com>
2025-12-30 00:33:38 +0000

    Adiciona métodos applyEvent e registerHandler à classe AggregateRoot, permitindo a aplicação de eventos de domínio e o registro de manipuladores de eventos. Isso melhora a estrutura de eventos dentro do domínio, facilitando a manipulação de eventos em entidades agregadas.

--- src/core/shared/domain/aggregate-root.ts
+++ src/core/shared/domain/aggregate-root.ts
@@ -1,3 +1,8 @@
+import { IDomainEvent } from '@core/shared/domain/events/domain-event.interface';
 import { Entity } from './entity';
 
-export abstract class AggregateRoot extends Entity {}
+export abstract class AggregateRoot extends Entity {
+  applyEvent(event: IDomainEvent) {}
+
+  registerHandler(event: string, handler: (event: IDomainEvent) => void) {}
+}
